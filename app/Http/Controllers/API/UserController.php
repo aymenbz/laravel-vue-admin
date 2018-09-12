93c5ed1f47fc45d7950fcf51de1a6eb92a9c6772
Aymen Bouzoumita <aymenweb5@gmail.com>
2018-09-12 14:49:43 +0100

    Upload and Convert Base64 String to Image

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -67,7 +67,10 @@ class UserController extends Controller
     public function updateProfile(Request $request)
     {
         $user = auth('api')->user();
-        return $request->photo;
+        if($request->photo) {
+            $name = time().'.' . explode('/', explode(':', substr($request->photo, 0, strpos($request->photo, ';')))[1])[1];
+            \Image::make($request->photo)->save(public_path('img/profile/').$name);
+        }
         // return ['message' => "sucess"];
     }
 
